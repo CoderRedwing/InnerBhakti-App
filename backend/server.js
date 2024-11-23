77c1c56a96d596935b03b62a1e18305d2bae6a7d
CoderRedwing <ajiteshru@gmail.com>
2024-11-23 11:43:47 +0530

    bug fixes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require('./config/db');
 const programRoutes = require('./routes/programRoutes');
 const audioRoutes = require('./routes/audioRoutes');
 const uploadRoutes = require('./routes/uploadRoutes');
+const http = require('http');  // Import http module
 
 // Load environment variables
 dotenv.config();
@@ -17,15 +18,16 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json({ limit: '20mb' }));
+
+// Create server instance and set timeout
+const server = http.createServer(app);
 server.setTimeout(30000); // 30 seconds
 
-
 // Routes
-
 app.use('/api/upload', uploadRoutes);
 app.use('/api/programs', programRoutes);
 app.use('/api/audio', audioRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
